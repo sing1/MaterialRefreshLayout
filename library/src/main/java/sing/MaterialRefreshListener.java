0c3530a2223903d0ce75ec0c584580ca6094a0ed
pc-PC\pc <15202653100@163.com>
2018-04-23 15:08:30 +0800

    no message

---
+++ library/src/main/java/sing/MaterialRefreshListener.java
@@ -0,0 +1,11 @@
+package sing;
+
+public abstract class MaterialRefreshListener {
+    public void onfinish() {
+    }
+
+    public abstract void onRefresh(MaterialRefreshLayout materialRefreshLayout);
+
+    public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+    }
+}
